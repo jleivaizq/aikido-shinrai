1b286a340d793b1d7e6d8a4522a01a91fdc9be91
Jose A. Leiva <jleivaizq@gmail.com>
2023-04-08 19:30:39 +0200

    Initial site with the default template

--- next.config.js
+++ next.config.js
@@ -1,6 +1,9 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   reactStrictMode: true,
+  images: {
+    domains: ["aikido-shinrai.local", "aikido-shinrai.000webhostapp.com"]
+  }
 }
 
 module.exports = nextConfig
